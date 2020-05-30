2f9994840a11b41dc07a8c4a7a47ebdfc8c0364d
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-05-29 23:36:04 -0400

    Finish task

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -4,6 +4,8 @@ void PointLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
+  const Eigen::Vector3d direction = p - q;
+  d = direction.normalized();
+  max_t = direction.norm();
   ////////////////////////////////////////////////////////////////////////////
 }
