43c9d25834b2f0b0bba34459b5539866c1525094
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-05-31 16:03:38 -0400

    Finish task

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -12,7 +12,39 @@ Eigen::Vector3d blinn_phong_shading(
   const std::vector<std::shared_ptr<Light> > & lights)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return Eigen::Vector3d(0,0,0);
+  Eigen::Vector3d color = Eigen::Vector3d(0,0,0);
+  Eigen::Vector3d ambient = Eigen::Vector3d(0,0,0);
+  Eigen::Vector3d diffuse = Eigen::Vector3d(0,0,0);
+  Eigen::Vector3d specular = Eigen::Vector3d(0,0,0);
+
+  const Eigen::Vector3d ka = objects[hit_id]->material->ka;
+  const Eigen::Vector3d kd = objects[hit_id]->material->kd;
+  const Eigen::Vector3d ks = objects[hit_id]->material->ks;
+  const double p = objects[hit_id]->material->phong_exponent;
+  Eigen::Vector3d h;
+
+  double max_t;
+  Ray light_ray;
+  light_ray.origin = ray.origin + t * ray.direction;
+  int hit_id_light;
+  double t_light;
+  Eigen::Vector3d n_light;
+
+  for (std::shared_ptr<Light> light: lights) {
+    light->direction(light_ray.origin, light_ray.direction, max_t);
+
+    if (!first_hit(ray, 1e-6, objects, hit_id_light, t_light, n_light) || t_light > max_t) {
+      ambient += (ka.array() * light->I.array()).matrix();
+      diffuse += (kd.array() * light->I.array() * std::max(0.0, n.dot(light_ray.direction))).matrix();
+      h = (-ray.direction.normalized() + light_ray.direction).normalized();
+      specular += (ks.array() * light->I.array() * std::max(0.0, n.dot(h))).matrix();
+    }
+    else
+    {
+    }
+  }
+
+  color = ambient + diffuse + specular;
+  return color;
   ////////////////////////////////////////////////////////////////////////////
 }
--- src/raycolor.cpp
+++ src/raycolor.cpp
@@ -12,7 +12,23 @@ bool raycolor(
   Eigen::Vector3d & rgb)
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  int hit_id;
+  double t;
+  Eigen::Vector3d n;
+  if (first_hit(ray, min_t, objects, hit_id, t, n)) {
+    rgb = blinn_phong_shading(ray, hit_id, t, n, objects, lights);
+    if (num_recursive_calls <= 10) {
+      Eigen::Vector3d rgb_temp;
+      Ray reflected_ray;
+      reflected_ray.origin = ray.origin + ray.direction * t;
+      reflected_ray.direction = reflect(ray.direction, n);
+      if (raycolor(reflected_ray, 1e-6, objects, lights, num_recursive_calls + 1, rgb_temp))
+      {
+        rgb += (objects[hit_id]->material->km.array() * rgb_temp.array()).matrix();
+      }
+    }
+    return true;
+  }
   rgb = Eigen::Vector3d(0,0,0);
   return false;
   ////////////////////////////////////////////////////////////////////////////
