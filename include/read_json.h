b4e3ab6b75db12c8e024cf7ea87f8b2c1dfc94e7
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-08-15 19:44:06 -0400

    Able to read .obj files

--- include/read_json.h
+++ include/read_json.h
@@ -1,13 +1,19 @@
 #ifndef READ_JSON_H
 #define READ_JSON_H
 
-#include <vector>
-#include <string>
 #include <memory>
+#include <string>
 #include <unordered_map>
+#include <vector>
 // Forward declaration
 struct Object;
 struct Light;
+
+// Get extention of file file_name
+std::string extension(std::string file_name) {
+  int position = file_name.find_last_of(".");
+  return file_name.substr(position + 1);
+}
 
 // Read a scene description from a .json file
 //
@@ -18,76 +24,72 @@ struct Light;
 //   objects  list of shared pointers to objects
 //   lights  list of shared pointers to lights
 inline bool read_json(
-  const std::string & filename, 
-  Camera & camera,
-  std::vector<std::shared_ptr<Object> > & objects,
-  std::vector<std::shared_ptr<Light> > & lights);
+    const std::string& filename,
+    Camera& camera,
+    std::vector<std::shared_ptr<Object> >& objects,
+    std::vector<std::shared_ptr<Light> >& lights);
 
 // Implementation
 
-#include <json.hpp>
-#include "readSTL.h"
-#include "dirname.h"
-#include "Object.h"
-#include "Sphere.h"
-#include "loop_subdivision.h"
-#include "Plane.h"
-#include "Triangle.h"
-#include "TriangleSoup.h"
-#include "Light.h"
-#include "PointLight.h"
-#include "DirectionalLight.h"
-#include "Material.h"
 #include <Eigen/Geometry>
+#include <cassert>
 #include <fstream>
 #include <iostream>
-#include <cassert>
+#include <json.hpp>
+
+#include "DirectionalLight.h"
+#include "Light.h"
+#include "Material.h"
+#include "Object.h"
+#include "Plane.h"
+#include "PointLight.h"
+#include "Sphere.h"
+#include "Triangle.h"
+#include "TriangleSoup.h"
+#include "dirname.h"
+#include "loop_subdivision.h"
+#include "readOBJ.h"
+#include "readSTL.h"
 
 inline bool read_json(
-  const std::string & filename, 
-  Camera & camera,
-  std::vector<std::shared_ptr<Object> > & objects,
-  std::vector<std::shared_ptr<Light> > & lights)
-{
+    const std::string& filename,
+    Camera& camera,
+    std::vector<std::shared_ptr<Object> >& objects,
+    std::vector<std::shared_ptr<Light> >& lights) {
   // Heavily borrowing from
   // https://github.com/yig/graphics101-raycasting/blob/master/parser.cpp
   using json = nlohmann::json;
 
-  std::ifstream infile( filename );
-  if( !infile ) return false;
+  std::ifstream infile(filename);
+  if (!infile) return false;
   json j;
   infile >> j;
 
-
   // parse a vector
-  auto parse_Vector3d = [](const json & j) -> Eigen::Vector3d
-  {
-    return Eigen::Vector3d(j[0],j[1],j[2]);
+  auto parse_Vector3d = [](const json& j) -> Eigen::Vector3d {
+    return Eigen::Vector3d(j[0], j[1], j[2]);
   };
   // parse camera
-  auto parse_camera = 
-    [&parse_Vector3d](const json & j, Camera & camera)
-  {
-    assert(j["type"] == "perspective" && "Only handling perspective cameras");
-    camera.d = j["focal_length"].get<double>();
-    camera.e =  parse_Vector3d(j["eye"]);
-    camera.v =  parse_Vector3d(j["up"]).normalized();
-    camera.w = -parse_Vector3d(j["look"]).normalized();
-    camera.u = camera.v.cross(camera.w);
-    camera.height = j["height"].get<double>();
-    camera.width = j["width"].get<double>();
-  };
-  parse_camera(j["camera"],camera);
+  auto parse_camera =
+      [&parse_Vector3d](const json& j, Camera& camera) {
+        assert(j["type"] == "perspective" && "Only handling perspective cameras");
+        camera.d = j["focal_length"].get<double>();
+        camera.e = parse_Vector3d(j["eye"]);
+        camera.v = parse_Vector3d(j["up"]).normalized();
+        camera.w = -parse_Vector3d(j["look"]).normalized();
+        camera.u = camera.v.cross(camera.w);
+        camera.height = j["height"].get<double>();
+        camera.width = j["width"].get<double>();
+      };
+  parse_camera(j["camera"], camera);
 
   // Parse materials
-  std::unordered_map<std::string,std::shared_ptr<Material> > materials;
+  std::unordered_map<std::string, std::shared_ptr<Material> > materials;
   auto parse_materials = [&parse_Vector3d](
-    const json & j,
-    std::unordered_map<std::string,std::shared_ptr<Material> > & materials)
-  {
+                             const json& j,
+                             std::unordered_map<std::string, std::shared_ptr<Material> >& materials) {
     materials.clear();
-    for(const json & jmat : j)
-    {
+    for (const json& jmat : j) {
       std::string name = jmat["name"];
       std::shared_ptr<Material> material(new Material());
       material->ka = parse_Vector3d(jmat["ka"]);
@@ -99,23 +101,19 @@ inline bool read_json(
       materials[name] = material;
     }
   };
-  parse_materials(j["materials"],materials);
+  parse_materials(j["materials"], materials);
 
   auto parse_lights = [&parse_Vector3d](
-    const json & j,
-    std::vector<std::shared_ptr<Light> > & lights)
-  {
+                          const json& j,
+                          std::vector<std::shared_ptr<Light> >& lights) {
     lights.clear();
-    for(const json & jlight : j)
-    {
-      if(jlight["type"] == "directional")
-      {
+    for (const json& jlight : j) {
+      if (jlight["type"] == "directional") {
         std::shared_ptr<DirectionalLight> light(new DirectionalLight());
         light->d = parse_Vector3d(jlight["direction"]).normalized();
         light->I = parse_Vector3d(jlight["color"]);
         lights.push_back(light);
-      }else if(jlight["type"] == "point")
-      {
+      } else if (jlight["type"] == "point") {
         std::shared_ptr<PointLight> light(new PointLight());
         light->p = parse_Vector3d(jlight["position"]);
         light->I = parse_Vector3d(jlight["color"]);
@@ -123,80 +121,91 @@ inline bool read_json(
       }
     }
   };
-  parse_lights(j["lights"],lights);
+  parse_lights(j["lights"], lights);
 
-  auto parse_objects = [&parse_Vector3d,&filename,&materials](
-    const json & j,
-    std::vector<std::shared_ptr<Object> > & objects)
-  {
+  auto parse_objects = [&parse_Vector3d, &filename, &materials](
+                           const json& j,
+                           std::vector<std::shared_ptr<Object> >& objects) {
     objects.clear();
-    for(const json & jobj : j)
-    {
-      if(jobj["type"] == "sphere")
-      {
+    for (const json& jobj : j) {
+      if (jobj["type"] == "sphere") {
         std::shared_ptr<Sphere> sphere(new Sphere());
         sphere->center = parse_Vector3d(jobj["center"]);
         sphere->radius = jobj["radius"].get<double>();
         objects.push_back(sphere);
-      }else if(jobj["type"] == "plane")
-      {
+      } else if (jobj["type"] == "plane") {
         std::shared_ptr<Plane> plane(new Plane());
         plane->point = parse_Vector3d(jobj["point"]);
         plane->normal = parse_Vector3d(jobj["normal"]).normalized();
         objects.push_back(plane);
-      }else if(jobj["type"] == "triangle")
-      {
+      } else if (jobj["type"] == "triangle") {
         std::shared_ptr<Triangle> tri(new Triangle());
         tri->corners = std::make_tuple(
-          parse_Vector3d(jobj["corners"][0]),
-          parse_Vector3d(jobj["corners"][1]),
-          parse_Vector3d(jobj["corners"][2]));
+            parse_Vector3d(jobj["corners"][0]),
+            parse_Vector3d(jobj["corners"][1]),
+            parse_Vector3d(jobj["corners"][2]));
         objects.push_back(tri);
-      }else if(jobj["type"] == "soup")
-      {
-        std::vector<std::vector<double> > V, SV;
-        std::vector<std::vector<int> > F, SF;
-        std::vector<std::vector<int> > N;
-        {
+      } else if (jobj["type"] == "soup") {
 #if defined(WIN32) || defined(_WIN32)
 #define PATH_SEPARATOR std::string("\\")
 #else
 #define PATH_SEPARATOR std::string("/")
 #endif
-          const std::string stl_path = jobj["stl"];
-          std::cout << stl_path << "\n";
-          igl::readSTL(
-              igl::dirname(filename)+
-              PATH_SEPARATOR +
-              stl_path,
-              V,F,N);
+        const std::string mesh_path = jobj["mesh"];
+
+        if (extension(mesh_path) == "stl") {
+          std::vector<std::vector<double> > V;
+          std::vector<std::vector<int> > F;
+          std::vector<std::vector<int> > N;
+          {
+            igl::readSTL(
+                igl::dirname(filename) +
+                    PATH_SEPARATOR +
+                    mesh_path,
+                V, F, N);
+          }
+          std::shared_ptr<TriangleSoup> soup(new TriangleSoup());
+          for (int f = 0; f < F.size(); f++) {
+            std::shared_ptr<Triangle> tri(new Triangle());
+            tri->corners = std::make_tuple(
+                Eigen::Vector3d(V[F[f][0]][0], V[F[f][0]][1], V[F[f][0]][2]),
+                Eigen::Vector3d(V[F[f][1]][0], V[F[f][1]][1], V[F[f][1]][2]),
+                Eigen::Vector3d(V[F[f][2]][0], V[F[f][2]][1], V[F[f][2]][2]));
+            soup->triangles.push_back(tri);
+          }
+          objects.push_back(soup);
+        } else {
+          Eigen::MatrixXd V_obj;
+          Eigen::MatrixXi F_obj;
+          {
+            igl::readOBJ(igl::dirname(filename) +
+                             PATH_SEPARATOR +
+                             mesh_path,
+                         V_obj, F_obj);
+          }
+          std::shared_ptr<TriangleSoup> soup(new TriangleSoup());
+          for (int f = 0; f < F_obj.rows(); f++) {
+            std::shared_ptr<Triangle> tri(new Triangle());
+            tri->corners = std::make_tuple(
+                Eigen::Vector3d(V_obj(F_obj(f, 0), 0), V_obj(F_obj(f, 0), 1), V_obj(F_obj(f, 0), 2)),
+                Eigen::Vector3d(V_obj(F_obj(f, 1), 0), V_obj(F_obj(f, 1), 1), V_obj(F_obj(f, 1), 2)),
+                Eigen::Vector3d(V_obj(F_obj(f, 2), 0), V_obj(F_obj(f, 2), 1), V_obj(F_obj(f, 2), 2)));
+            soup->triangles.push_back(tri);
+          }
+          objects.push_back(soup);
         }
-        std::shared_ptr<TriangleSoup> soup(new TriangleSoup());
-        for(int f = 0;f<SF.size();f++)
-        {
-          std::shared_ptr<Triangle> tri(new Triangle());
-          tri->corners = std::make_tuple(
-            Eigen::Vector3d( SV[SF[f][0]][0], SV[SF[f][0]][1], SV[SF[f][0]][2]),
-            Eigen::Vector3d( SV[SF[f][1]][0], SV[SF[f][1]][1], SV[SF[f][1]][2]),
-            Eigen::Vector3d( SV[SF[f][2]][0], SV[SF[f][2]][1], SV[SF[f][2]][2])
-          );
-          soup->triangles.push_back(tri);
-        }
-        objects.push_back(soup);
       }
       //objects.back()->material = default_material;
-      if(jobj.count("material"))
-      {
-        if(materials.count(jobj["material"]))
-        {
+      if (jobj.count("material")) {
+        if (materials.count(jobj["material"])) {
           objects.back()->material = materials[jobj["material"]];
         }
       }
     }
   };
-  parse_objects(j["objects"],objects);
-
+  parse_objects(j["objects"], objects);
+  
   return true;
 }
 
-#endif 
+#endif
